60374793399cda4773fc6e6980abc76190cb5ba4
dudddy <zernin.egor2077@yandex.ru>
2022-09-27 00:35:53 +0300

    improvement

--- Lesson 1/tic_tac_toe.py
+++ Lesson 1/tic_tac_toe.py
@@ -3,17 +3,16 @@
 Консольная игра крестики-нолики в виде класса
 """
 
-import sys
-
 WALL = "#############"
 PARTITION = "#---+---+---#"
+CROSS = 'X'
+TOE = '0'
 
 
 class TicTacGame:
     """
-    TicTacGame, в будущем покрытый тестами
+        тесты будут, нормальный .pylintrc тоже
     """
-
     def __init__(self):
         self.board = list(range(1, 10))
         self.win_combinations = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7),
@@ -38,9 +37,9 @@ class TicTacGame:
             move = int(move)
             if move == 0:
                 print("Спасибо, что зашли к нам!")
-                sys.exit()
+                return True
             if str(self.board[move - 1]) in 'X0':
-                print(f"Ячейка занята {self.board[move - 1]}. Выберите другую")
+                print(f"Ячейка уже занята {self.board[move - 1]}. Выберите другую")
                 continue
             self.board[move - 1] = player
             print()
@@ -51,8 +50,11 @@ class TicTacGame:
         player_turn = 'X'
         while True:
             self.show_board()
-            self.validate_input(player_turn)
-            self.check_winner(player_turn)
+            flag_exit = self.validate_input(player_turn)
+            flag_end = self.check_winner(player_turn)
+
+            if flag_exit or flag_end:
+                break
 
             if player_turn == 'X':
                 player_turn = '0'
@@ -60,23 +62,30 @@ class TicTacGame:
                 player_turn = 'X'
 
     def check_winner(self, player_turn):
-        p_t = player_turn
         self.counter += 1
 
         for win in self.win_combinations:
-            if self.board[win[0] - 1] == p_t and self.board[win[1] - 1] == p_t and self.board[win[2] - 1] == p_t:
+            if self.board[win[0] - 1] == player_turn and self.board[win[1] - 1] == player_turn \
+                    and self.board[win[2] - 1] == player_turn:
                 self.show_board()
-                print("\n" + player_turn, r"победили!!! \(★ω★)/")
-                sys.exit()
+                print("\n" + player_turn, r"победили!!! \(★ω★)/" + "\nХотите сыграть еще раз? (Да/Нет)")
+                self.decision_to_play()
+                return True
         if self.counter == 9:
             self.show_board()
-            print("Игра завершилась ничьей. Сыграть еще раз? (Да/Любое другое слово, значющее отказ (не матерное))")
-            if input() == "Да":
-                self.board = list(range(1, 10))
-                self.start_game()
-            else:
-                print("Пока!")
-                sys.exit()
+            print("\nИгра завершилась ничьей. Сыграть еще раз? (Да/Любое другое слово, значущее отказ (не матерное))")
+            self.decision_to_play()
+            return True
+
+        return False
+
+    def decision_to_play(self):
+        if input() == "Да":
+            self.board = list(range(1, 10))
+            self.counter = 0
+            self.start_game()
+        else:
+            print("Пока!")
 
 
 if __name__ == '__main__':
